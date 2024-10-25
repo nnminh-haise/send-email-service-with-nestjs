6f9799b53a65ba98a56f9e54a870b2cd605be056
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-10-25 13:26:03 +0700

    update: configured basic settings and added dotenv package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const applicationPort: string = process.env.APP_PORT;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestApplication<any> = await NestFactory.create(AppModule);
+  await app.listen(applicationPort);
 }
 bootstrap();
