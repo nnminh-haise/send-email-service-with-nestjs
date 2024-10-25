e7439ca3bd64e190d6783c544df1988c2bc59ab6
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-10-25 13:53:34 +0700

    update: added user and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmailModule } from './email/email.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [EmailModule],
+  imports: [EmailModule, UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
